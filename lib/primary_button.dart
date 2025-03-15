16740d5e8a11067791e0d607329495eccbf6fa56
ArunPrajapati <aprajapati950@gmail.com>
2025-03-15 14:03:08 +0530

    Update primary_button.dart

--- lib/primary_button.dart
+++ lib/primary_button.dart
@@ -2,10 +2,15 @@ import 'package:flutter/material.dart';
 import '../commonui.dart';
 import 'common_margin.dart';
 
-class PrimaryButton extends StatelessWidget {
+enum ButtonVariant {
+  primary,
+  secondary,
+  tertiary,
+}
+
+class AppButton extends StatefulWidget {
   final Widget label;
-  final Color? color;
-  final double? maxWidth;
+  final ButtonVariant variant;
   final VoidCallback onClick;
   final String? prefixIconPath;
   final String? suffixIconPath;
@@ -13,11 +18,10 @@ class PrimaryButton extends StatelessWidget {
   final Color? suffixIconColor;
   final bool isDisabled;
 
-  const PrimaryButton({
+  const AppButton({
     required this.label,
     required this.onClick,
-    this.color,
-    this.maxWidth,
+    this.variant = ButtonVariant.primary,
     this.prefixIconPath,
     this.suffixIconPath,
     this.prefixIconColor,
@@ -27,27 +31,46 @@ class PrimaryButton extends StatelessWidget {
   });
 
   @override
-  Widget build(final BuildContext context) {
+  _AppButtonState createState() => _AppButtonState();
+}
+
+class _AppButtonState extends State<AppButton> {
+  bool _isPressed = false;
+
+  @override
+  Widget build(BuildContext context) {
     return GestureDetector(
-      onTap: isDisabled
+      onTap: widget.isDisabled
           ? null
-          : onClick, // Disable button on tap if isDisabled is true
+          : () {
+              setState(() {
+                _isPressed = true;
+              });
+              widget.onClick(); // Call the onClick callback
+            },
+      onTapUp: (_) {
+        setState(() {
+          _isPressed = false;
+        });
+      },
       child: Container(
-        constraints: BoxConstraints(maxWidth: maxWidth ?? 640),
         decoration: BoxDecoration(
-          color: isDisabled
-              ? color ?? Theme.of(context).appColors.neutral100
-              : Theme.of(context).appColors.primary500,
+          color: _getBackgroundColor(context),
           borderRadius: BorderRadius.circular(12),
+          border: _getBorder(context),
         ),
         child: Padding(
-          padding: const EdgeInsets.symmetric(vertical: 12, horizontal: 20),
+          padding: EdgeInsets.symmetric(
+            vertical: 12,
+            horizontal: MediaQuery.of(context).size.width > 400 ? 24 : 16,
+          ),
           child: Row(
+            mainAxisSize: MainAxisSize.min,  // Wraps content based on label and icons
             mainAxisAlignment: MainAxisAlignment.center,
             children: [
-              _buildIcon(context, prefixIconPath, prefixIconColor),
-              label,
-              _buildIcon(context, suffixIconPath, suffixIconColor),
+              _buildIcon(context, widget.prefixIconPath, widget.prefixIconColor),
+              widget.label,
+              _buildIcon(context, widget.suffixIconPath, widget.suffixIconColor),
             ],
           ),
         ),
@@ -55,8 +78,49 @@ class PrimaryButton extends StatelessWidget {
     );
   }
 
-  Widget _buildIcon(final BuildContext context, final String? iconPath,
-      final Color? iconColor,) {
+  Color _getBackgroundColor(BuildContext context) {
+    if (widget.isDisabled) {
+      return widget.variant == ButtonVariant.primary
+          ? Theme.of(context).appColors.neutral100
+          : widget.variant == ButtonVariant.secondary
+              ? Theme.of(context).appColors.neutral200
+              : Theme.of(context).appColors.neutral300;
+    }
+
+    if (_isPressed) {
+      // Slightly darker shade when button is pressed
+      return widget.variant == ButtonVariant.primary
+          ? Theme.of(context).appColors.primary400 // Darker primary color
+          : widget.variant == ButtonVariant.secondary
+              ? Theme.of(context).appColors.secondary400 // Darker secondary color
+              : Theme.of(context).appColors.neutral400; // Darker tertiary color
+    }
+
+    switch (widget.variant) {
+      case ButtonVariant.primary:
+        return Theme.of(context).appColors.primary500;
+      case ButtonVariant.secondary:
+        return Theme.of(context).appColors.secondary500;
+      case ButtonVariant.tertiary:
+        return Colors.transparent; // No background color for tertiary
+    }
+  }
+
+  Border? _getBorder(BuildContext context) {
+    if (widget.variant == ButtonVariant.tertiary) {
+      return Border.all(
+        color: Theme.of(context).appColors.neutral500,
+        width: 2,
+      );
+    }
+    return null; // No border for primary or secondary
+  }
+
+  Widget _buildIcon(
+    final BuildContext context,
+    final String? iconPath,
+    final Color? iconColor,
+  ) {
     if (iconPath == null) {
       return const SizedBox();
     }
